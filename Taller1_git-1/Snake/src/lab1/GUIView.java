3a42c3afadc229bb1569a1e3d64b27a6d1e3cc4f
djsabando <douglas_sabando@hotmail.es>
2020-10-22 12:52:43 -0500

    Integrante 3

--- Taller1_git-1/Snake/src/lab1/GUIView.java
+++ Taller1_git-1/Snake/src/lab1/GUIView.java
@@ -59,11 +59,11 @@ public class GUIView extends JPanel {
 		this.guiPanel = new JPanel();
 
 		// Set the background on that panel
-		this.guiPanel.setBackground(Color.lightGray);
+		this.guiPanel.setBackground(Color.Gray);
 
 		// Create a new button on that panel and add a StartGameListener as
 		// listener on that button
-		this.startGameButton = new JButton("Jugar");
+		this.startGameButton = new JButton("Empezar");
 		this.startGameButton.addActionListener(new StartGameListener());
 		this.guiPanel.add(this.startGameButton);
 
@@ -111,3 +111,4 @@ public class GUIView extends JPanel {
 		}
 	}
 }
+
